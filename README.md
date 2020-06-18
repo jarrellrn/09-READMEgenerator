57c1cbf8ef4554ac1782fd61cf87156583b1b97f
jarrellrn <jarrellrn@gmail.com>
2020-06-18 13:10:40 -0500

    added more code and functionality

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-# readme generator
+# README generator
 
 ## Description
- this application can be used to generate a functioning markdown readme## Table of contents
+ This application generates a readable markdown readme based on user input.
+## Table of contents
 1. [Installation](#installation)
 2. [Usage](#usage)
 3. [License](#license)
 4. [Contributing](#contributing)
 5. [Tests](#tests)
 6. [Questions](#questions)
+
+### Installation: <a name="installation"></a>
+ Once downloaded, go into the main directory folder and run 'npm i' to install the dependencies.
+### Usage: <a name="usage"></a>
+ Once dependencies are installed, simply run 'node index.js' in the main directory.
+### License: <a name="license"></a>
+ ISC
+### Contributing: <a name="contributing"></a>
+ Robert Jarrell
+### Tests: <a name="tests"></a>
+ N/A
+### Questions: <a name="questions"></a>
+If you have any questions or concerns, my github is jarrellrn. Or you can email me at jarrellrn@gmail.com.
--- index.js
+++ index.js
@@ -46,15 +46,23 @@ function runInq(){
   ])
   .then(function(answers){
 
-    var data = `# ${answers.title}\n\n` +
-    `## Description\n ${answers.description}` +
+    var data =
+    `# ${answers.title}\n\n` +
+    `## Description\n ${answers.description}\n` +
     `## Table of contents\n` +
     `1. [Installation](#installation)\n` +
     `2. [Usage](#usage)\n` +
     `3. [License](#license)\n` +
     `4. [Contributing](#contributing)\n` +
     `5. [Tests](#tests)\n` +
-    `6. [Questions](#questions)\n`
+    `6. [Questions](#questions)\n\n` +
+    `### Installation: <a name="installation"></a>\n ${answers.installation}\n` +
+    `### Usage: <a name="usage"></a>\n ${answers.usage}\n` +
+    `### License: <a name="license"></a>\n ${answers.license}\n` +
+    `### Contributing: <a name="contributing"></a>\n ${answers.contributing}\n` +
+    `### Tests: <a name="tests"></a>\n ${answers.tests}\n` +
+    `### Questions: <a name="questions"></a>\n` +
+    `If you have any questions or concerns, my github is ${answers.questionsUsername}. Or you can email me at ${answers.questionsEmail}.`
 
 
 
